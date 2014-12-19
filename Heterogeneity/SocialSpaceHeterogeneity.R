16a54a8aedb1ac77bc5dad8bf9f39106a641f04d
sztal <sztal111@gmail.com>
2014-12-19 02:09:52 +0100

    Add "SocialSpaceHeterogeneity.R" (finished; but more detailed analysis is in the report)

--- Heterogeneity/SocialSpaceHeterogeneity.R
+++ Heterogeneity/SocialSpaceHeterogeneity.R
@@ -7,6 +7,7 @@ library(lattice)
 library(latticeExtra)
 library(reshape2)
 library(lmtest)
+library(lme4)
 source("HelperFunctionsMisc/ComputingMisc.R")
 
 # Load the data
@@ -26,7 +27,7 @@ xyplot(value ~ ent_avg | variable, data=E,
        }) # there are perhaps some outliers;
 
 # Check correlation of ent_avg with places to see if it is important to control for it (and also check correlations with attachment scales)
-lowerCor(D[,c(2, 12:13, 4:7)]) # no strong correlations
+lowerCor(D[,c(2:3, 12, 4:7)]) # no strong correlations
 
 # Save square of ent_avg for testing models with quadratic term
 D$ent_avg2 = D$ent_avg^2
@@ -48,6 +49,14 @@ raintest(resmob.lm1) # and the relationship is linear; that is also good
 dwtest(resmob.lm1) # no autocorrelations
 # Outliers
 outlierTest(resmob.lm1) # it seems there is not strong outliers...
+# Check the quadratic model
+resmob.lm2 = lm(resmob~ent_avg+I(ent_avg^2), data=D)
+# Diagnostics
+shapiro.test(resmob.lm2$residuals)
+bptest(resmob.lm2)
+raintest(resmob.lm2)
+dwtest(resmob.lm2)
+outlierTest(resmob.lm2)
 
 # Test interactions effects with attachments scales
 # Attachment and resmob - correlations
@@ -64,13 +73,21 @@ outlierTest(resmob.gen)
 # But the general fit is poor, since the distributon is very not normal
 
 # Interaction with place discovered
-resmob.disc = lm(resmob ~ ent_avg*attdiscovered, data=D)
-# no significatn interaction term
+resmob.disc = lm(resmob ~ (ent_avg+I(ent_avg^2))*attdiscovered, data=D)
+# significatn interaction term
 shapiro.test(resmob.disc$residuals)
 bptest(resmob.disc) # heteroscedasticity problem
 raintest(resmob.disc)
 dwtest(resmob.disc)
 outlierTest(resmob.disc)
+# Visualization
+xyplot(resmob ~ ent_avg, data=D,
+       panel = function(x, y) {
+             panel.xyplot(x, y)
+             panel.loess(x, y, col="red")
+             panel.curve(6.1799+7.3954*x-3.2294*x^2)
+             panel.abline(h=mean(y), lwd=.5, lty=2)
+       })
 
 
 # Soccont
@@ -91,6 +108,7 @@ xyplot(soccont ~ ent_avg, data=D,
              panel.xyplot(x, y)
              panel.loess(x, y, col="red")
              panel.curve(6.645+8.004*x-4.999*x^2)
+             panel.abline(h=mean(y), lwd=.5, lty=2)
        })
 
 # Interactions with attgen and attdiscovered
